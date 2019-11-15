3796f1708253ccdc38d67b6134e0b2e3170e2897
Simon Freedland <simon.freedland@nology.io>
2019-11-15 08:30:06 +0000

    deployed app to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB_XVE1xQ9MY-IP5fHvYmBA4YKamZHNHiU",
+    authDomain: "strava-visualiser.firebaseapp.com",
+    databaseURL: "https://strava-visualiser.firebaseio.com",
+    projectId: "strava-visualiser",
+    storageBucket: "strava-visualiser.appspot.com",
+    messagingSenderId: "477051364294",
+    appId: "1:477051364294:web:6f6afbdfcbad4aad0348c7",
+    measurementId: "G-4GHQWJVGP5"
+  }
 };
